56ff0af0f5dc3e3848ad218b2927e38770abdada
kavinda-ravishan <kvnravishan@outlook.com>
2021-09-29 23:11:40 +0530

    ANN for regression

--- A_deep_understanding_of_deep_learning_PyTorch_Cpp/Application.cpp
+++ A_deep_understanding_of_deep_learning_PyTorch_Cpp/Application.cpp
@@ -1,42 +1,44 @@
 #include "pch.h"
 
-namespace Gradient_Descent {
-
-	torch::Tensor fx(torch::Tensor& x) {
-		return (3 * x.pow(2)) - (3 * x) + 4;
-	}
-
-	torch::Tensor deriv(torch::Tensor& x) {
-		return (6 * x) - 3;
-	}
-
-	void Gradient_descent_in_1D(torch::Tensor(*deriv)(torch::Tensor& x)) {
-
-		int x_len = 2001;
-		torch::Tensor x = torch::linspace(-2, 2, x_len);
-		torch::Tensor localMin = x[torch::randint(0, x_len, 1).item<int>()];
-
-		std::cout << localMin << std::endl;
-
-		int training_epochs = 100;
-		torch::Tensor learning_rate = torch::tensor(0.01, torch::kFloat64);
-		torch::Tensor grad = torch::tensor(0, torch::kFloat64);
-
-		for (int i = 0; i < training_epochs; i++) {
-
-			grad = deriv(localMin);
-			localMin = localMin - (learning_rate * grad);
-		}
-
-		std::cout << localMin << std::endl;
-	}
-}
-
-using namespace Gradient_Descent;
-
 int main(int argc, char** args) {
 
-	Gradient_descent_in_1D(deriv);
+#pragma region Create data
+
+	int N = 30;
+
+	torch::Tensor x = torch::randn({ N,1 }, torch::kFloat32);
+	torch::Tensor y = x + torch::randn({ N,1 }, torch::kFloat32)/2;
+
+#pragma endregion
+
+	torch::nn::Sequential ANNReg(
+		torch::nn::Linear(1,1),
+		torch::nn::ReLU(),
+		torch::nn::Linear(1,1)
+	);
+
+	float learningRate = 0.05;
+	torch::optim::SGD optimizer(ANNReg->parameters(), learningRate);
+
+	int numepochs = 500;
+	torch::Tensor losses = torch::zeros(numepochs, torch::kFloat32);
+
+	for (int i = 0; i < numepochs; i++) {
+		torch::Tensor yhat = ANNReg->forward(x);
+
+		torch::Tensor loss = torch::mse_loss(yhat, y);
+		losses[i] = loss;
+
+		optimizer.zero_grad();
+		loss.backward();
+		optimizer.step();
+	}
+	
+	torch::Tensor predictions = ANNReg->forward(x);
+
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+	std::cout << predictions << std::endl;
 
 	return 0;
 }
--- A_deep_understanding_of_deep_learning_PyTorch_Cpp/Torch_Functions.cpp
+++ A_deep_understanding_of_deep_learning_PyTorch_Cpp/Torch_Functions.cpp
@@ -307,3 +307,35 @@ namespace Math_numpy_PyTorch {
 		Reproducible_randomness_seeding();
 	}
 }
+
+namespace Gradient_Descent {
+
+	torch::Tensor fx(torch::Tensor& x) {
+		return (3 * x.pow(2)) - (3 * x) + 4;
+	}
+
+	torch::Tensor deriv(torch::Tensor& x) {
+		return (6 * x) - 3;
+	}
+
+	void Gradient_descent_in_1D(torch::Tensor(*deriv)(torch::Tensor& x)) {
+
+		int x_len = 2001;
+		torch::Tensor x = torch::linspace(-2, 2, x_len);
+		torch::Tensor localMin = x[torch::randint(0, x_len, 1).item<int>()];
+
+		std::cout << localMin << std::endl;
+
+		int training_epochs = 100;
+		torch::Tensor learning_rate = torch::tensor(0.01, torch::kFloat64);
+		torch::Tensor grad = torch::tensor(0, torch::kFloat64);
+
+		for (int i = 0; i < training_epochs; i++) {
+
+			grad = deriv(localMin);
+			localMin = localMin - (learning_rate * grad);
+		}
+
+		std::cout << localMin << std::endl;
+	}
+}
