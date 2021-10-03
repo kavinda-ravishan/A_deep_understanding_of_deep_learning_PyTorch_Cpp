7c460f54036f89a5af5495e92b80b3a4715afc85
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-03 21:47:17 +0530

    Multilayer_ANN_classification

--- A_deep_understanding_of_deep_learning_PyTorch_Cpp/Application.cpp
+++ A_deep_understanding_of_deep_learning_PyTorch_Cpp/Application.cpp
@@ -19,35 +19,39 @@ void plot_data(
 	//for (auto a : ax) std::cout << a << std::endl;
 
 	RGBA s1Color{ 0,0,1,1 }; // R,G,B,A
-	RGBA s2Color{ 1,0,0,1 }; // R,G,B,A
+	RGBA s2Color{ 0,1,0,1 }; // R,G,B,A
 
 	RGBABitmapImageReference* imageReference = CreateRGBABitmapImageReference();
 
-	ScatterPlotSeries* series1 = GetDefaultScatterPlotSeriesSettings();
-	series1->xs = &ax;
-	series1->ys = &ay;
-	series1->linearInterpolation = false;
-	series1->pointType = toVector(L"dots");
-	series1->color = &s1Color;
-
-	ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
-	series2->xs = &bx;
-	series2->ys = &by;
-	series2->linearInterpolation = false;
-	series2->pointType = toVector(L"dots");
-	series2->color = &s2Color;
-
 	ScatterPlotSettings* settings = GetDefaultScatterPlotSettings();
-	settings->width = 600;
-	settings->height = 400;
+	settings->width = 800;
+	settings->height = 600;
 	settings->autoBoundaries = true;
 	settings->autoPadding = true;
 	settings->title = toVector(L"Data");
 	settings->xLabel = toVector(L"dimension 1");
 	settings->yLabel = toVector(L"dimension 2");
-	settings->scatterPlotSeries->push_back(series1);
-	settings->scatterPlotSeries->push_back(series2);
-
+	
+	if (ax.size() != 0 && ay.size() != 0) {
+		ScatterPlotSeries* series1 = GetDefaultScatterPlotSeriesSettings();
+		settings->scatterPlotSeries->push_back(series1);
+		series1->xs = &ax;
+		series1->ys = &ay;
+		series1->linearInterpolation = false;
+		series1->pointType = toVector(L"dots");
+		series1->color = &s1Color;
+	}
+	
+	if (bx.size() != 0 && by.size() != 0) {
+		ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
+		settings->scatterPlotSeries->push_back(series2);
+		series2->xs = &bx;
+		series2->ys = &by;
+		series2->linearInterpolation = false;
+		series2->pointType = toVector(L"dots");
+		series2->color = &s2Color;
+	}
+	
 	DrawScatterPlotFromSettings(imageReference, settings);
 
 	vector<double>* pngdata = ConvertToPNG(imageReference->image);
@@ -73,39 +77,46 @@ void plot_data_err(
 
 	RGBABitmapImageReference* imageReference = CreateRGBABitmapImageReference();
 
-	ScatterPlotSeries* series1 = GetDefaultScatterPlotSeriesSettings();
-	series1->xs = &ax;
-	series1->ys = &ay;
-	series1->linearInterpolation = false;
-	series1->pointType = toVector(L"dots");
-	series1->color = &s1Color;
-
-	ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
-	series2->xs = &bx;
-	series2->ys = &by;
-	series2->linearInterpolation = false;
-	series2->pointType = toVector(L"dots");
-	series2->color = &s2Color;
-
-	ScatterPlotSeries* series3 = GetDefaultScatterPlotSeriesSettings();
-	series3->xs = &errx;
-	series3->ys = &erry;
-	series3->linearInterpolation = false;
-	series3->pointType = toVector(L"crosses");
-	series3->color = &s3Color;
 
 	ScatterPlotSettings* settings = GetDefaultScatterPlotSettings();
-	settings->width = 600;
-	settings->height = 400;
+	settings->width = 800;
+	settings->height = 600;
 	settings->autoBoundaries = true;
 	settings->autoPadding = true;
 	settings->title = toVector(L"Data");
 	settings->xLabel = toVector(L"dimension 1");
 	settings->yLabel = toVector(L"dimension 2");
-	settings->scatterPlotSeries->push_back(series1);
-	settings->scatterPlotSeries->push_back(series2);
-	settings->scatterPlotSeries->push_back(series3);
 
+	if (ax.size() != 0 && ay.size() != 0) {
+		ScatterPlotSeries* series1 = GetDefaultScatterPlotSeriesSettings();
+		settings->scatterPlotSeries->push_back(series1);
+		series1->xs = &ax;
+		series1->ys = &ay;
+		series1->linearInterpolation = false;
+		series1->pointType = toVector(L"dots");
+		series1->color = &s1Color;
+	}
+	
+	if (bx.size() != 0 && by.size() != 0) {
+		ScatterPlotSeries* series2 = GetDefaultScatterPlotSeriesSettings();
+		settings->scatterPlotSeries->push_back(series2);
+		series2->xs = &bx;
+		series2->ys = &by;
+		series2->linearInterpolation = false;
+		series2->pointType = toVector(L"dots");
+		series2->color = &s2Color;
+	}
+	
+	if (errx.size() != 0 && erry.size() != 0) {
+		ScatterPlotSeries* series3 = GetDefaultScatterPlotSeriesSettings();
+		settings->scatterPlotSeries->push_back(series3);
+		series3->xs = &errx;
+		series3->ys = &erry;
+		series3->linearInterpolation = false;
+		series3->pointType = toVector(L"crosses");
+		series3->color = &s3Color;
+	}
+	
 	DrawScatterPlotFromSettings(imageReference, settings);
 
 	vector<double>* pngdata = ConvertToPNG(imageReference->image);
@@ -113,7 +124,7 @@ void plot_data_err(
 	DeleteImage(imageReference->image);
 }
 
-void plot_losses(torch::Tensor losses_t, int numepochs) {
+void plot_losses(torch::Tensor losses_t, int numepochs, std::string path) {
 
 	std::vector<double>losses (losses_t.data_ptr<float>(), losses_t.data_ptr<float>() + losses_t.numel());
 	std::vector<double> epochs(numepochs);
@@ -144,7 +155,7 @@ void plot_losses(torch::Tensor losses_t, int numepochs) {
 	DrawScatterPlotFromSettings(imageReference, settings);
 
 	vector<double>* pngdata = ConvertToPNG(imageReference->image);
-	WriteToFile(pngdata, "plots/ANN_classifier_losses.png");
+	WriteToFile(pngdata, path);
 	DeleteImage(imageReference->image);
 }
 
@@ -169,7 +180,7 @@ void ANN_classification() {
 	std::vector<double> bx(bx_t.data_ptr<float>(), bx_t.data_ptr<float>() + bx_t.numel());
 	std::vector<double> by(by_t.data_ptr<float>(), by_t.data_ptr<float>() + by_t.numel());
 
-	plot_data(ax, ay, bx, by, "plots/ANN_classifier.png");
+	plot_data(ax, ay, bx, by, "plots/ANN_classifier_1.png");
 
 	torch::Tensor labels = torch::vstack({ torch::zeros({nPerClust, 1}), torch::ones({nPerClust, 1}) });
 
@@ -211,7 +222,7 @@ void ANN_classification() {
 
 	torch::Tensor predictions = ANNclassify->forward(data);
 
-	plot_losses(losses, numepochs);
+	plot_losses(losses, numepochs, "plots/ANN_classifier_3_losses.png");
 
 	int errors = 0;
 	bool y = false;
@@ -220,6 +231,8 @@ void ANN_classification() {
 	vector<double> errx;
 	vector<double> erry;
 
+	float threshold = 0.5;
+
 	for (int i = 0; i < nPerClust * 2; i++) {
 
 		y = false;
@@ -227,7 +240,7 @@ void ANN_classification() {
 
 		if (labels[i].item<int>() == 1)
 			y = true;
-		if (predictions[i].item<float>() > 0.5)
+		if (predictions[i].item<float>() > threshold)
 			yHat = true;
 
 		if (y != yHat) {
@@ -246,7 +259,7 @@ void ANN_classification() {
 
 	for (int i = 0; i < nPerClust * 2; i++) {
 
-		if (predictions[i].item<float>() < 0.5) {
+		if (predictions[i].item<float>() <= threshold) {
 			catAx.push_back(data[i][0].item<double>());
 			catAy.push_back(data[i][1].item<double>());
 		}
@@ -256,7 +269,125 @@ void ANN_classification() {
 		}
 	}
 
-	plot_data_err(catAx, catAy, catBx, catBy, errx, erry, "plots/ANN_classifier_pred.png");
+	plot_data_err(catAx, catAy, catBx, catBy, errx, erry, "plots/ANN_classifier_2_pred.png");
+
+#pragma endregion
+}
+
+void Multilayer_ANN_classification() {
+
+#pragma region Create data
+
+	int nPerClust = 100;
+	int blur = 1;
+
+	int A[] = { 1, 3  }; // centroid category 1
+	int	B[] = { 1, -2 }; // centroid category 2
+
+
+	torch::Tensor ax_t = A[0] + torch::randn({ nPerClust }, torch::TensorOptions(torch::kCPU).dtype(at::kFloat)) * blur;
+	torch::Tensor ay_t = A[1] + torch::randn({ nPerClust }, torch::TensorOptions(torch::kCPU).dtype(at::kFloat)) * blur;
+	torch::Tensor bx_t = B[0] + torch::randn({ nPerClust }, torch::TensorOptions(torch::kCPU).dtype(at::kFloat)) * blur;
+	torch::Tensor by_t = B[1] + torch::randn({ nPerClust }, torch::TensorOptions(torch::kCPU).dtype(at::kFloat)) * blur;
+
+	std::vector<double> ax(ax_t.data_ptr<float>(), ax_t.data_ptr<float>() + ax_t.numel());
+	std::vector<double> ay(ay_t.data_ptr<float>(), ay_t.data_ptr<float>() + ay_t.numel());
+	std::vector<double> bx(bx_t.data_ptr<float>(), bx_t.data_ptr<float>() + bx_t.numel());
+	std::vector<double> by(by_t.data_ptr<float>(), by_t.data_ptr<float>() + by_t.numel());
+
+	plot_data(ax, ay, bx, by, "plots/Multilayer_ANN_classifier_1.png");
+
+	torch::Tensor labels = torch::vstack({ torch::zeros({nPerClust, 1}), torch::ones({nPerClust, 1}) });
+
+	torch::Tensor dataA = torch::transpose(torch::stack({ ax_t, ay_t }), 1, 0);
+	torch::Tensor dataB = torch::transpose(torch::stack({ bx_t, by_t }), 1, 0);
+
+	torch::Tensor data = torch::vstack({ dataA, dataB });
+
+#pragma endregion
+
+#pragma region creating and train the model
+
+	torch::nn::Sequential ANNclassify(
+		torch::nn::Linear(2, 16),
+		torch::nn::ReLU(),
+		torch::nn::Linear(16, 1),
+		torch::nn::ReLU(),
+		torch::nn::Linear(1, 1)
+	);
+
+	float learningRate = 0.01;
+	torch::optim::SGD optimizer(ANNclassify->parameters(), learningRate);
+
+	int numepochs = 1000;
+	torch::Tensor losses = torch::zeros(numepochs, torch::kFloat32);
+
+	for (int i = 0; i < numepochs; i++) {
+		torch::Tensor yhat = ANNclassify->forward(data);
+
+		torch::Tensor loss = torch::binary_cross_entropy_with_logits(yhat, labels);
+		losses[i] = loss;
+
+		optimizer.zero_grad();
+		loss.backward();
+		optimizer.step();
+	}
+#pragma endregion
+
+#pragma region make predictions
+
+	torch::Tensor predictions = ANNclassify->forward(data);
+
+	plot_losses(losses, numepochs, "plots/Multilayer_ANN_classifier_3_losses.png");
+
+	int errors = 0;
+	bool y = false;
+	bool yHat = false;
+
+	vector<double> errx;
+	vector<double> erry;
+
+	float threshold = 0;
+
+	for (int i = 0; i < nPerClust * 2; i++) {
+
+		y = false;
+		yHat = false;
+
+		if (labels[i].item<int>() == 1)
+			y = true;
+		if (predictions[i].item<float>() > threshold)
+			yHat = true;
+
+		if (y != yHat) {
+
+			errx.push_back(data[i][0].item<double>());
+			erry.push_back(data[i][1].item<double>());
+			errors++;
+		}
+	}
+
+	std::cout << "Accuracy : " << 100 * (((nPerClust * 2) - errors) / float(nPerClust * 2)) << "%" << std::endl;
+
+	vector<double> catAx;
+	vector<double> catAy;
+	vector<double> catBx;
+	vector<double> catBy;
+
+	for (int i = 0; i < nPerClust * 2; i++) {
+
+		if (predictions[i].item<float>() <= threshold) {
+			catAx.push_back(data[i][0].item<double>());
+			catAy.push_back(data[i][1].item<double>());
+		}
+		else {
+			catBx.push_back(data[i][0].item<double>());
+			catBy.push_back(data[i][1].item<double>());
+		}
+	}
+
+
+	plot_data_err(catAx, catAy, catBx, catBy, errx, erry, "plots/Multilayer_ANN_classifier_2_pred.png");
 
 #pragma endregion
 }
@@ -267,9 +398,11 @@ int main(int argc, char** args) {
 	Math_numpy_PyTorch::AllCalls();
 	Gradient_Descent::AllCalls();
 	ANNs::AllCalls();
+	
+	ANN_classification();
 	*/
 
-	ANN_classification();
+	Multilayer_ANN_classification();
 
 	return 0;
 }
