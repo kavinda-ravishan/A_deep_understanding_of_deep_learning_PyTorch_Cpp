2e8982067cc98559cf97a3bc3d0b0de6ae485373
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-30 18:03:56 +0530

    Transferring_Model_from_Python

--- A_deep_understanding_of_deep_learning_PyTorch_Cpp/pch.h
+++ A_deep_understanding_of_deep_learning_PyTorch_Cpp/pch.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <torch/torch.h>
 #include "pbPlots.hpp"
 #include "supportLib.hpp"
--- Getting_Started_with_PyTorch_Cpp/Application.cpp
+++ Getting_Started_with_PyTorch_Cpp/Application.cpp
@@ -1,30 +1,8 @@
-#include "torch/torch.h"
+ #include "Network.h"
 
-struct NetImpl : torch::nn::Module {
+#include "torch/script.h" // Transferring_Model_from_Python
 
-	torch::nn::Linear fc1{ nullptr }, fc2{ nullptr }, out{ nullptr };
-
-	NetImpl(int fc1_dims, int fc2_dims)
-		:fc1(fc1_dims, fc1_dims), fc2(fc1_dims, fc2_dims), out(fc2_dims, 1)
-	{
-		register_module("fc1", fc1);
-		register_module("fc2", fc2);
-		register_module("out", out);
-	}
-
-	torch::Tensor forward(torch::Tensor x)
-	{
-		x = torch::relu(fc1(x));
-		x = torch::relu(fc2(x));
-		x = torch::relu(out(x));
-
-		return x;
-	}
-};
-
-TORCH_MODULE(Net);
-
-int main(int argc, char** args) {
+void Building_a_Neural_Network() {
 
 	Net network(50, 10);
 	std::cout << network << std::endl;
@@ -35,7 +13,32 @@ int main(int argc, char** args) {
 
 	output = network->forward(x);
 	std::cout << output << std::endl;
+}
 
+void Transferring_Model_from_Python() {
+
+	torch::jit::script::Module net = torch::jit::load("./python/net.pt");
+
+	torch::Tensor x = torch::randn({ 1, 100 });
+
+	std::vector<torch::jit::IValue> input;
+
+	input.push_back(x);
+
+	torch::jit::IValue out = net.forward(input);
+
+	std::cout << x << std::endl;
+	std::cout << std::endl;
+	std::cout << out << std::endl;
+	std::cout << std::endl;
+	std::cout << typeid(out).name() << std::endl;
+}
+
+int main(int argc, char** args) {
+
+	Building_a_Neural_Network();
+	Transferring_Model_from_Python();
+	
 	return 0;
 }
 
