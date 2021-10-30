e02e7750065276e5453decc947d5c7e93bf9edb9
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-30 19:54:19 +0530

    config opencv.

--- Getting_Started_with_PyTorch_Cpp/Application.cpp
+++ Getting_Started_with_PyTorch_Cpp/Application.cpp
@@ -1,43 +1,10 @@
- #include "Network.h"
-
-#include "torch/script.h" // Transferring_Model_from_Python
-
-void Building_a_Neural_Network() {
-
-	Net network(50, 10);
-	std::cout << network << std::endl;
-
-	torch::Tensor x, output;
-
-	x = torch::randn({ 2, 50 });
-
-	output = network->forward(x);
-	std::cout << output << std::endl;
-}
-
-void Transferring_Model_from_Python() {
-
-	torch::jit::script::Module net = torch::jit::load("./python/net.pt");
-
-	torch::Tensor x = torch::randn({ 1, 100 });
-
-	std::vector<torch::jit::IValue> input;
-
-	input.push_back(x);
-
-	torch::jit::IValue out = net.forward(input);
-
-	std::cout << x << std::endl;
-	std::cout << std::endl;
-	std::cout << out << std::endl;
-	std::cout << std::endl;
-	std::cout << typeid(out).name() << std::endl;
-}
+#include <torch/torch.h>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char** args) {
 
-	Building_a_Neural_Network();
-	Transferring_Model_from_Python();
+	torch::Tensor;
+	cv::Mat a;
 	
 	return 0;
 }
--- Getting_Started_with_PyTorch_Cpp/Network.h
+++ Getting_Started_with_PyTorch_Cpp/Network.h
@@ -1,3 +1,4 @@
+/*
 #pragma once
 
 #include "torch/torch.h"
@@ -25,3 +26,40 @@ struct NetImpl : torch::nn::Module {
 };
 
 TORCH_MODULE(Net);
+
+#include "Network.h"
+
+#include "torch/script.h" // Transferring_Model_from_Python
+
+void Building_a_Neural_Network() {
+
+	Net network(50, 10);
+	std::cout << network << std::endl;
+
+	torch::Tensor x, output;
+
+	x = torch::randn({ 2, 50 });
+
+	output = network->forward(x);
+	std::cout << output << std::endl;
+}
+
+void Transferring_Model_from_Python() {
+
+	torch::jit::script::Module net = torch::jit::load("./python/net.pt");
+
+	torch::Tensor x = torch::randn({ 1, 100 });
+
+	std::vector<torch::jit::IValue> input;
+
+	input.push_back(x);
+
+	torch::jit::IValue out = net.forward(input);
+
+	std::cout << x << std::endl;
+	std::cout << std::endl;
+	std::cout << out << std::endl;
+	std::cout << std::endl;
+	std::cout << typeid(out).name() << std::endl;
+}
+*/
