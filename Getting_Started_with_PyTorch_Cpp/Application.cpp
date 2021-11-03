6c992be13159a92d9eef4d28ba0055abe3a94e38
kavinda-ravishan <kvnravishan@outlook.com>
2021-11-03 18:38:30 +0530

    read image with opencv

--- Getting_Started_with_PyTorch_Cpp/Application.cpp
+++ Getting_Started_with_PyTorch_Cpp/Application.cpp
@@ -1,93 +1,151 @@
 #include "pch.h"
 
-#define DEFAULT_WIDTH 720
-#define DEFAULT_HEIGHT 1280
-#define IMG_SIZE 512
+constexpr int kTrainSize = 8000;
+constexpr int kTestSize = 2000;
 
-cv::Mat frame_prediction(cv::Mat frame, torch::jit::Module model)
+constexpr int kRows = 300;
+constexpr int kCols = 300;
+
+torch::Tensor CVtoTensor(cv::Mat img)
+{}
+std::pair<torch::Tensor, torch::Tensor> read_data(const std::string& root, bool train)
 {
-	double alpha = 0.4;
-	double beta = 1 - alpha;
-	cv::Mat frame_copy, dst;
-	std::vector<torch::jit::IValue> input;
-	std::vector<double> mean = { 0.406, 0.456, 0.485 };
-	std::vector<double> std = { 0.225, 0.224, 0.229 };
+	int i = 0;
+	std::string ext(".jpg");
+	const auto num_samples = train ? kTrainSize : kTestSize;
+	const auto folder = train ? root + "/train" : root + "/test";
+	auto targets = torch::empty(num_samples, torch::kInt64);
+	auto images = torch::empty({ num_samples, 3, kRows, kCols }, torch::kFloat);
 
-	cv::resize(frame, frame, cv::Size(IMG_SIZE, IMG_SIZE));
-	frame_copy = frame;
-	frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
+	std::string cat_folder = folder + "/cats";
+	std::string dog_folder = folder + "/dogs";
+	std::vector<std::string> folders = { cat_folder, dog_folder };
 
-	//cv to tensor
-	torch::Tensor frame_tensor = torch::from_blob(frame.data, { 1, IMG_SIZE, IMG_SIZE, 3 });
-	frame_tensor = frame_tensor.permute({ 0, 3, 1, 2 }); // { 1, 3, IMG_SIZE, IMG_SIZE}
-
-	frame_tensor = torch::data::transforms::Normalize<>(mean, std)(frame_tensor);
-	frame_tensor = frame_tensor.to(torch::kCPU);
-
-	input.push_back(frame_tensor);
-
-	auto pred = model.forward(input).toTensor().detach().to(torch::kCPU);
-	pred = pred.mul(100).clamp(0, 255).to(torch::kU8);
-
-	cv::Mat output_mat(cv::Size(IMG_SIZE, IMG_SIZE), CV_8UC1, pred.data_ptr());
-	cv::cvtColor(output_mat, output_mat, cv::COLOR_GRAY2RGB);
-	cv::applyColorMap(output_mat, output_mat, cv::COLORMAP_TWILIGHT_SHIFTED);
-
-	cv::addWeighted(frame_copy, alpha, output_mat, beta, 0.0, dst);
-	cv::resize(dst, dst, cv::Size(DEFAULT_WIDTH, DEFAULT_HEIGHT));
-
-	return dst;
-}
-
-torch::jit::Module load_model(std::string model_name)
-{
-	std::string dir = "./models/" + model_name;
-	torch::jit::Module module = torch::jit::load(dir);
-	module.to(torch::kCPU);
-	module.eval();
-	std::cout << "MODEL LOADED.." << std::endl;
-
-	return module;
-}
-
-
-int main(int argc, char** args) {
-
-	torch::jit::script::Module module;
-
-	cv::VideoCapture cap;
-	cv::Mat frame;
-	cap.open("./videos/driving_1.mp4");
-
-	if (!cap.isOpened()) {
-		std::cout << "Can not open the video file!!" << std::endl;
-	}
-	std::cout << "Video file opened.." << std::endl;
-
-
-	try {
-		module = load_model("quantized_lanesNet.pt");
-	}
-	catch (const c10::Error& e) {
-		std::cerr << "ERROR: MODEL DID NOT LOAD!!\n";
-	}
-
-	std::cout << "Press Esc to terminate" << std::endl;
-
-	while (true)
+	for (auto& f : folders)
 	{
-		cap.read(frame);
-		if (frame.empty()) {
-			std::cerr << "Error: Blank frame!!\n";
-			break;
+		/*
+		for (const auto& p : fs::directory_iterator(f))
+		{
+			if (p.path().extension() == ext)
+			{
+				cv::Mat img = cv::imread(p.path());
+				auto img_tensor = CVtoTensor(img);
+				images[i] = img_tensor;
+			}
+		}
+		*/
+
+		return { images, targets };
+	}
+}
+
+struct CatGog: torch::data::datasets::Dataset<CatGog>
+{
+public:
+	enum Mode { kTrain, kTest };
+
+	explicit CatGog(const std::string& root, Mode mode = Mode::kTrain) :mode_(mode)
+	{
+		auto data = read_data(root, (mode == Mode::kTrain));
+		images_ = std::move(data.first);
+		targets_ = std::move(data.second);
+	}
+	torch::data::Example<> get(size_t index) override
+	{
+		return { images_[index], targets_[index] };
+	}
+	torch::optional<size_t> size() const override
+	{}
+	bool is_train() const noexcept
+	{}
+	const torch::Tensor& images() const
+	{}
+	const torch::Tensor& targets() const
+	{}
+private:
+	torch::Tensor images_;
+	torch::Tensor targets_;
+	Mode mode_;
+};
+
+
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+
+bool ListFiles(std::wstring path, std::wstring mask, std::vector<std::wstring>& files) {
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA ffd;
+	std::wstring spec;
+	std::stack<std::wstring> directories;
+
+	directories.push(path);
+	files.clear();
+
+	while (!directories.empty()) {
+		path = directories.top();
+		spec = path + L"\\" + mask;
+		directories.pop();
+
+		hFind = FindFirstFile(spec.c_str(), &ffd);
+		if (hFind == INVALID_HANDLE_VALUE) {
+			return false;
 		}
 
-		frame = frame_prediction(frame, module);
-		cv::imshow("video", frame);
+		do {
+			if (wcscmp(ffd.cFileName, L".") != 0 &&
+				wcscmp(ffd.cFileName, L"..") != 0) {
+				if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+					directories.push(path + L"\\" + ffd.cFileName);
+				}
+				else {
+					files.push_back(path + L"\\" + ffd.cFileName);
+				}
+			}
+		} while (FindNextFile(hFind, &ffd) != 0);
 
-		if (cv::waitKey(10) == 27) break;
+		if (GetLastError() != ERROR_NO_MORE_FILES) {
+			FindClose(hFind);
+			return false;
+		}
+
+		FindClose(hFind);
+		hFind = INVALID_HANDLE_VALUE;
+	}
+
+	return true;
+}
+
+
+int main(int argc, char** args) 
+{
+	std::vector<std::wstring> files;
+
+	if (ListFiles(L"E:\\Projects\\Git_Projects\\A_deep_understanding_of_deep_learning_PyTorch_Cpp\\Getting_Started_with_PyTorch_Cpp\\dataset\\train\\dogs", L"*", files)) {
+		for (std::vector<std::wstring>::iterator it = files.begin();
+			it != files.end();
+			++it) {
+			std::wcout << it->c_str() << std::endl;
+			std::wstring wstr = it->c_str();
+
+			std::string str(wstr.begin(), wstr.end());
+			std::cout << str << std::endl;
+			cv::Mat img = cv::imread(str);
+			cv::imshow("win", img);
+			cv::waitKey();
+			
+		}
+
+		
 	}
 
 	return 0;
 }
+
+
+
+
 
