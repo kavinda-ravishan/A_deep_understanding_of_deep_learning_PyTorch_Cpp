63ff6948f65e268337c462268e99917d62a6c93d
kavinda-ravishan <kvnravishan@outlook.com>
2021-11-04 18:04:48 +0530

    read images and convert to tensors.

--- Getting_Started_with_PyTorch_Cpp/Application.cpp
+++ Getting_Started_with_PyTorch_Cpp/Application.cpp
@@ -1,80 +1,19 @@
 #include "pch.h"
 
-constexpr int kTrainSize = 8000;
-constexpr int kTestSize = 2000;
+constexpr int kTrainSize = 80;
+constexpr int kTestSize = 20;
 
 constexpr int kRows = 300;
 constexpr int kCols = 300;
 
 torch::Tensor CVtoTensor(cv::Mat img)
-{}
-std::pair<torch::Tensor, torch::Tensor> read_data(const std::string& root, bool train)
 {
-	int i = 0;
-	std::string ext(".jpg");
-	const auto num_samples = train ? kTrainSize : kTestSize;
-	const auto folder = train ? root + "/train" : root + "/test";
-	auto targets = torch::empty(num_samples, torch::kInt64);
-	auto images = torch::empty({ num_samples, 3, kRows, kCols }, torch::kFloat);
-
-	std::string cat_folder = folder + "/cats";
-	std::string dog_folder = folder + "/dogs";
-	std::vector<std::string> folders = { cat_folder, dog_folder };
-
-	for (auto& f : folders)
-	{
-		/*
-		for (const auto& p : fs::directory_iterator(f))
-		{
-			if (p.path().extension() == ext)
-			{
-				cv::Mat img = cv::imread(p.path());
-				auto img_tensor = CVtoTensor(img);
-				images[i] = img_tensor;
-			}
-		}
-		*/
-
-		return { images, targets };
-	}
+	cv::resize(img, img, cv::Size(kRows, kCols), 0, 0, cv::INTER_LINEAR);
+	cv::cvtColor(img, img, cv::COLOR_RGB2RGBA);
+	auto img_tensor = torch::from_blob(img.data, { kRows, kCols, 3 }, torch::kByte);
+	img_tensor = img_tensor.permute({ 2, 0, 1 }).toType(torch::kFloat).div_(255);
+	return img_tensor;
 }
-
-struct CatGog: torch::data::datasets::Dataset<CatGog>
-{
-public:
-	enum Mode { kTrain, kTest };
-
-	explicit CatGog(const std::string& root, Mode mode = Mode::kTrain) :mode_(mode)
-	{
-		auto data = read_data(root, (mode == Mode::kTrain));
-		images_ = std::move(data.first);
-		targets_ = std::move(data.second);
-	}
-	torch::data::Example<> get(size_t index) override
-	{
-		return { images_[index], targets_[index] };
-	}
-	torch::optional<size_t> size() const override
-	{}
-	bool is_train() const noexcept
-	{}
-	const torch::Tensor& images() const
-	{}
-	const torch::Tensor& targets() const
-	{}
-private:
-	torch::Tensor images_;
-	torch::Tensor targets_;
-	Mode mode_;
-};
-
-
-#include <windows.h>
-#include <string>
-#include <vector>
-#include <stack>
-#include <iostream>
-
 
 bool ListFiles(std::wstring path, std::wstring mask, std::vector<std::wstring>& files) {
 	HANDLE hFind = INVALID_HANDLE_VALUE;
@@ -119,28 +58,82 @@ bool ListFiles(std::wstring path, std::wstring mask, std::vector<std::wstring>& 
 	return true;
 }
 
+std::pair<torch::Tensor, torch::Tensor> read_data(const std::string& root, bool train)
+{
+	int i = 0;
+	std::wstring ext(L"*.jpg");
+	const auto num_samples = train ? kTrainSize : kTestSize;
+	const auto folder = train ? root + "/train" : root + "/test";
+	auto targets = torch::empty(num_samples, torch::kInt64);
+	auto images = torch::empty({ num_samples, 3, kRows, kCols }, torch::kFloat);
+
+	std::string cat_folder = folder + "/cats";
+	std::string dog_folder = folder + "/dogs";
+	std::vector<std::string> folders = { cat_folder, dog_folder };
+
+	std::vector<std::wstring> files;
+
+	int64_t label = 0;
+	for (auto& f : folders)
+	{
+		std::wstring path(f.begin(), f.end());
+		if (ListFiles(path, ext, files)) {
+			for (std::vector<std::wstring>::iterator it = files.begin(); it != files.end(); ++it)
+			{
+				std::cout << label << " : " << i << std::endl;
+
+				std::wstring wstr = it->c_str();
+
+				std::string str(wstr.begin(), wstr.end());
+				cv::Mat img = cv::imread(str);
+				auto img_tensor = CVtoTensor(img);
+				images[i] = img_tensor;
+				targets[i] = torch::tensor(label, torch::kInt64);
+				i++;
+			}
+		}
+		label++;
+	}
+
+	return { images, targets };
+}
+
+struct CatGog: torch::data::datasets::Dataset<CatGog>
+{
+public:
+	enum Mode { kTrain, kTest };
+
+	explicit CatGog(const std::string& root, Mode mode = Mode::kTrain) :mode_(mode)
+	{
+		auto data = read_data(root, (mode == Mode::kTrain));
+		images_ = std::move(data.first);
+		targets_ = std::move(data.second);
+	}
+	torch::data::Example<> get(size_t index) override
+	{
+		return { images_[index], targets_[index] };
+	}
+	torch::optional<size_t> size() const override
+	{}
+	bool is_train() const noexcept
+	{}
+	const torch::Tensor& images() const
+	{}
+	const torch::Tensor& targets() const
+	{}
+private:
+	torch::Tensor images_;
+	torch::Tensor targets_;
+	Mode mode_;
+};
 
 int main(int argc, char** args) 
 {
-	std::vector<std::wstring> files;
-
-	if (ListFiles(L"E:\\Projects\\Git_Projects\\A_deep_understanding_of_deep_learning_PyTorch_Cpp\\Getting_Started_with_PyTorch_Cpp\\dataset\\train\\dogs", L"*", files)) {
-		for (std::vector<std::wstring>::iterator it = files.begin();
-			it != files.end();
-			++it) {
-			std::wcout << it->c_str() << std::endl;
-			std::wstring wstr = it->c_str();
-
-			std::string str(wstr.begin(), wstr.end());
-			std::cout << str << std::endl;
-			cv::Mat img = cv::imread(str);
-			cv::imshow("win", img);
-			cv::waitKey();
-			
-		}
-
-		
-	}
+	/*
+	const std::string root = "E:/Projects/Git_Projects/A_deep_understanding_of_deep_learning_PyTorch_Cpp/Getting_Started_with_PyTorch_Cpp/dataset";
+	bool train = false;
+	std::pair<torch::Tensor, torch::Tensor> data = read_data(root, !train);
+	*/
 
 	return 0;
 }
--- Getting_Started_with_PyTorch_Cpp/pch.h
+++ Getting_Started_with_PyTorch_Cpp/pch.h
@@ -1,5 +1,6 @@
 #pragma once
-
 #include <torch/script.h>
 #include <torch/torch.h>
 #include <opencv2/opencv.hpp>
+#include <windows.h>
+#include <stack>
